faa89544bd2b5a42be660f00c37aad725637e5c6
catalunha <catalunha.mj@gmail.com>
2023-05-02 14:34:09 -0300

    usado para revisar o classfrase

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+catalunha@pop-os:~/myapp/learning_about_b4a_dart_speed$ dart bin/learning_about_b4a_dart_speed.dart 
--- bin/learning_about_b4a_dart_speed.dart
+++ bin/learning_about_b4a_dart_speed.dart
@@ -5,19 +5,31 @@ void main(List<String> arguments) async {
   print('Start');
   await initialize();
   QueryBuilder<ParseObject> query =
-      QueryBuilder<ParseObject>(ParseObject('Table3'));
+      QueryBuilder<ParseObject>(ParseObject('Phrase'));
   // query.keysToReturn(['colA']);
-  query.keysToReturn(['colA', 'typePointerTable4.colA']);
-  // query.includeObject(['typePointerTable4']);
+  query.keysToReturn(['user.profile']);
+  query.includeObject(['user']);
+  query.orderByAscending('objectId');
+  query.setLimit(1000);
+
   // query.selectKeys('colA', '3a');
   var parseResponse = await query.query();
 
   if (parseResponse.success && parseResponse.results != null) {
     for (var element in parseResponse.results!) {
       ParseObject parseObject = element as ParseObject;
-      print(parseObject);
-      print(parseObject.get('colA'));
-      print(parseObject.get('typePointerTable4'));
+      print(parseObject.objectId);
+      // print(parseObject.get('colA'));
+      print(parseObject.get('user').get('profile').objectId);
+
+      final parseObjectPhrase = ParseObject('Phrase');
+      parseObjectPhrase.objectId = parseObject.objectId;
+      parseObjectPhrase.set(
+          'userProfile',
+          (ParseObject('UserProfile')
+                ..objectId = parseObject.get('user').get('profile').objectId)
+              .toPointer());
+      await parseObjectPhrase.save();
       print('=-=-=');
     }
   }
@@ -25,8 +37,8 @@ void main(List<String> arguments) async {
 }
 
 Future<void> initialize() async {
-  String appId = 'x4uHx8hJECtAPiKo4Z2f8IK9OUKRtcuUBBaUBHM9';
-  String clientKey = 'GoHjfXQ1fIClRQ6K9Gg9hc1dpKj90HJGRXcHeSrs';
+  String appId = '';
+  String clientKey = '';
   String serverUrl = 'https://parseapi.back4app.com';
   await Parse().initialize(
     appId,
